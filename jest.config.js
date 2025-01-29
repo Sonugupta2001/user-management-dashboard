01f4514f5cc388646edd5c4af318332b8eb9da41
Sonu Gupta <sg6263709700@gmail.com>
2025-01-29 18:47:59 +0530

    refactoring components

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
-    moduleNameMapper: {
-      '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-      '\\.(gif|ttf|eot|svg|jpg|png)$': '<rootDir>/__mocks__/fileMock.js',
-    },
-  };
